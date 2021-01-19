911f6fc849445603778aa06e5283bccf17876fa0
ErialdoDFfreitas <erialdodomingosplay@gmail.com>
2021-01-19 15:54:05 -0300

    Initial commit

---
+++ ServCE/src/main/java/com/insight/api/ServCeApplication.java
@@ -0,0 +1,12 @@
+package com.insight.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServCeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServCeApplication.class, args);
+	}
+
+}
---
+++ ServCE/src/test/java/com/example/insight/ServCeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServCeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
